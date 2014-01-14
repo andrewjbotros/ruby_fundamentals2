2111045b4fa84b2a05c147bd065a6d301963dcc9
andrewjbotros <andrewjbotros@gmail.com>
2014-01-14 14:18:20 -0500

    Exercise7 pump up the jams

--- exercise7.rb
+++ exercise7.rb
@@ -1,5 +1,24 @@
+#add students
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+#display students
+def display (students)
+	students.each do |key, value|
+    puts "#{key}: #{value}"
+	end
+end
+
+#print students
+puts "*print students"
+display (students)
+
+#add cohort
+puts "*add cohort"
+students[:cohort4] = 43
+
+#print students again
+display (students)
