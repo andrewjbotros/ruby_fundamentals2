8087f150d96b098cf46b7dffcbbf9d0c9f31a7f6
andrewjbotros <andrewjbotros@gmail.com>
2014-01-14 14:40:25 -0500

    Exercise7 final commit

--- exercise7.rb
+++ exercise7.rb
@@ -22,3 +22,23 @@ students[:cohort4] = 43
 
 #print students again
 display (students)
+
+#increase cohort size by 5%
+puts "increase cohort size by 5%"
+students.each{ |key,value| students[key] = "#{(value*1.05).to_i}" }
+display (students)
+
+#delete the 2nd cohort
+puts "delete 2nd cohort, return hash"
+students.delete(:cohort2)
+display (students) 
+
+#count total number of students
+puts "count total number of students in all cohorts"
+
+sum = 0
+students.each do |key, value|
+	sum += value.to_i
+end
+
+puts sum
